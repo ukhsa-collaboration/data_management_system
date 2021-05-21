eba9d3f41ea7d78e49adfa2904822b4995155d5f
Joe <joseph.catling@phe.gov.uk>
2021-05-21 11:25:33 +0100

    change

--- db/migrate/20210521102230_add_project_dataset_levels_table.rb
+++ db/migrate/20210521102230_add_project_dataset_levels_table.rb
@@ -2,12 +2,10 @@
 class AddProjectDatasetLevelsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :project_dataset_levels do |t|
-      t.integer :project_dataset_id
-      t.integer :level
+      t.references :project_dataset, foreign_key: true
+      t.integer :access_level_id
       t.date :expiry_date
       t.boolean :approved
     end
-
-    add_foreign_key :project_dataset_levels, :project_datasets, column: :project_dataset_id
   end
 end
--- db/structure.sql
+++ db/structure.sql
@@ -27,37 +27,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: access_levels; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.access_levels (
-    id bigint NOT NULL,
-    value character varying,
-    created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL
-);
-
-
---
--- Name: access_levels_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.access_levels_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: access_levels_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.access_levels_id_seq OWNED BY public.access_levels.id;
-
-
---
 -- Name: addresses; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -4441,13 +4410,6 @@ CREATE TABLE public.zuser (
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.access_levels ALTER COLUMN id SET DEFAULT nextval('public.access_levels_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.addresses ALTER COLUMN id SET DEFAULT nextval('public.addresses_id_seq'::regclass);
 
 
@@ -5163,14 +5125,6 @@ ALTER TABLE ONLY public.z_team_statuses ALTER COLUMN id SET DEFAULT nextval('pub
 --
 
 ALTER TABLE ONLY public.z_user_statuses ALTER COLUMN id SET DEFAULT nextval('public.z_user_statuses_id_seq'::regclass);
-
-
---
--- Name: access_levels_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.access_levels
-    ADD CONSTRAINT access_levels_pkey PRIMARY KEY (id);
 
 
 --
@@ -8158,8 +8112,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210506093309'),
 ('20210518103646'),
 ('20210518150518'),
-('20210519144851'),
-('20210519161222'),
-('20210519161356');
+('20210519144851');
 
 
