fba3094948d8fe8168e96fdf2eed21557fd264a5
Joe <joseph.catling@phe.gov.uk>
2021-06-08 17:24:35 +0100

    add clearing of expiry dates

--- app/javascript/controllers/cas_form_controller.js
+++ app/javascript/controllers/cas_form_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["level", "default_checkbox"]
+  static targets = ["level", "default_checkbox", "expiry_date"]
   check_using_roles() {
     const master_checked = event.currentTarget.checked
     for(var i = 0; i < this.default_checkboxTargets.length; i++){
       this.default_checkboxTargets[i].checked = false;
+    };
+    for(var i = 0; i < this.expiry_dateTargets.length; i++){
+      this.expiry_dateTargets[i].value = "";
     };
     event.currentTarget.checked = master_checked;
     for(var i = 0; i < this.levelTargets.length; i++){
