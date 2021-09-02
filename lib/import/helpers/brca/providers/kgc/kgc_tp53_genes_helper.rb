6b847921c3ebee8d07db2b41b22ef5ebb0453def
Francesco <francesco.santaniello@phe.gov.uk>
2021-09-02 12:13:49 +0100

    #Refactored KGC brca provider and helper scripts

---
+++ lib/import/helpers/brca/providers/kgc/kgc_tp53_genes_helper.rb
@@ -0,0 +1,98 @@
+require_relative 'kgc_constants'
+require_relative 'kgc_helper'
+
+module Import
+  module Helpers
+    module Brca
+      module Providers
+        module Kgc
+          # Processing methods for Non lynch genes
+          module KgcTp53GenesHelper
+            include KgcConstants
+            include KgcHelper
+
+            def process_tp53_entries(raw_genotype, clinicomm, genotype, genotypes)
+              if raw_genotype == 'No mutation detected'
+                add_negative_test_for(%w[BRCA1 BRCA2 TP53], genotypes, genotype,
+                                      NEGATIVE_TEST_LOG)
+              elsif raw_genotype.scan(BRCA_GENES_REGEX).count.positive?
+                mutatedgene = raw_genotype.scan(BRCA_GENES_REGEX).flatten
+                
+                process_tp53_genes(raw_genotype, clinicomm, genotype, genotypes,
+                                             mutatedgene)
+              elsif /no mutation|No mutation detected|Normal result/i.match(raw_genotype)
+                tp53genes = tp53_genes_from(clinicomm)
+                @logger.debug "Found no mutation; Genes #{nonlynchgenes.flatten.uniq} are normal"
+                add_negative_test_for(tp53genes.flatten.uniq, genotypes, genotype,
+                                      NEGATIVE_TEST_LOG)
+              end
+            end
+
+            def process_tp53_genes(raw_genotype, clinicomm, genotype, genotypes,
+                                             mutatedgene)
+              if raw_genotype.scan(CDNA_REGEX).count.positive? && raw_genotype !~ EXON_REGEX
+                process_tp53_cdna_change(raw_genotype, mutatedgene, clinicomm,
+                                                       genotype, genotypes)
+              elsif EXON_REGEX.match(raw_genotype) && raw_genotype !~ CDNA_REGEX
+                process_tp53_exon(raw_genotype, clinicomm, genotype, genotypes)
+              elsif EXON_REGEX.match(raw_genotype) && CDNA_REGEX.match(raw_genotype)
+                process_tp53_exon_and_cdna_change(raw_genotype, clinicomm, genotype,
+                                                       genotypes)
+              end
+            end
+
+            def process_tp53_cdna_change(raw_genotype, mutatedgene, clinicomm, genotype,
+                                              genotypes)
+              mutatedcdna    = raw_genotype.scan(CDNA_REGEX).flatten
+              mutatedprotein = raw_genotype.scan(PROTEIN_REGEX_COLO).flatten
+              if raw_genotype.scan(CDNA_REGEX).count > 1 && mutatedgene.flatten.one?
+                mutatedgene = mutatedgene.map { |mutatedgene| [mutatedgene] * 2 }
+              end
+              mutations      = mutatedgene.flatten.zip(mutatedcdna, mutatedprotein)
+              @logger.debug "Found dna mutation in #{mutatedgene} " \
+                            "GENE(s) in position #{raw_genotype.scan(CDNA_REGEX)} with impact " \
+                            "#{raw_genotype.scan(PROTEIN_REGEX)}"
+              process_mutated_genes(mutations, genotype, genotypes)
+              tp53genes = tp53_genes_from(clinicomm)
+              negativegenes = tp53genes.flatten.uniq - mutatedgene.flatten
+              process_negative_genes(negativegenes, genotypes, genotype,
+                                     NEGATIVE_LOG)
+            end
+
+            def process_tp53_exon(raw_genotype, clinicomm, genotype, genotypes)
+              @logger.debug 'Found NON-LYNCH CHROMOSOME ' \
+                            "#{EXON_REGEX.match(raw_genotype)[:deldupins]} "\
+                            "in #{BRCA_GENES_REGEX.match(raw_genotype)[:colorectal]} " \
+                            'NON-LYNCH GENE at '\
+                            "position #{EXON_REGEX.match(raw_genotype)[:exno]}"
+              mutatedgene   = raw_genotype.scan(BRCA_GENES_REGEX).flatten
+              tp53genes = tp53_genes_from(clinicomm)
+              negativegenes = tp53genes.flatten.uniq - mutatedgene
+              process_negative_genes(negativegenes, genotypes, genotype,
+                                     NEGATIVE_TEST_LOG)
+              result = { gene: brca_gene_from(raw_genotype), exon: exon_from(raw_genotype),
+                         variant: variant_from(raw_genotype) }
+              add_result_to(genotype, genotypes, result)
+            end
+
+            def process_tp53_exon_and_cdna_change(raw_genotype, clinicomm, genotype,
+                                                       genotypes)
+              mutatedgene   = raw_genotype.scan(BRCA_GENES_REGEX).flatten
+              tp53genes = tp53_genes_from(clinicomm)
+              negativegenes = nonlynchgenes.flatten.uniq - mutatedgene
+              process_negative_genes(negativegenes, genotypes, genotype,
+                                     NEGATIVE_TEST_LOG)
+              mutatedexongenotype = genotype.dup
+              add_mutated_result_to(mutatedexongenotype, raw_genotype, genotypes)
+
+              result = { gene: raw_genotype.scan(BRCA_GENES_REGEX)[1].join,
+                         gene_location: gene_location_from(raw_genotype),
+                         protein: protein_impact_from(raw_genotype) }
+              add_result_to(genotype, genotypes, result)
+            end
+          end
+        end
+      end
+    end
+  end
+end
